903153dbbf03b6dc485f1aa21230e77ad4802afa
ritik dave <ritik.dave@intimetec.com>
2024-12-18 12:35:49 +0530

    first commit

---
+++ Assignment1/Assignment1/YearlyCarbonEmmisionUtills.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public static class YearlyCarbonEmmisionUtills
+    {
+        public static double CarbonEmmissionForSpamEmailStorage(double sizeInMB)
+        {
+            return 5 * sizeInMB;
+        }
+
+        public static double CarbonEmmisionForEmailTransmission(double sizeInMB)
+        {
+            return 30 * sizeInMB;
+
+        }
+
+        public static double CarbonEmmisionForEmailStorage(double sizeInMB)
+        {
+            return 10 * sizeInMB;
+
+        }
+
+        
+    }
+}
