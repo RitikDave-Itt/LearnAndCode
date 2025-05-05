d68ca3d80208817612d0bf44a835b48f4147b434
ritikdave <ritik.dave@intimetec.com>
2025-05-05 14:40:01 +0530

    Location Finder Assignment

---
+++ LocationFinder/LocationFinder/Utilities/ApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LocationFinder.Utilities.Exceptions;
+
+namespace LocationFinder.Utilities
+{
+    public static class ApiClient
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task<string> GetAsync(string url)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new NullResponseException();
+            }
+        }
+    }
+}
---
+++ LocationFinder/LocationFinder/Utilities/Exceptions/NullResponseException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LocationFinder.Utilities.Exceptions
+{
+    public class NullResponseException : Exception
+    {
+        public NullResponseException()
+            : base("The response from the API was null.")
+        {
+        }
+
+        public NullResponseException(string message)
+            : base(message)
+        {
+        }
+
+        public NullResponseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+        
+    }
+}
