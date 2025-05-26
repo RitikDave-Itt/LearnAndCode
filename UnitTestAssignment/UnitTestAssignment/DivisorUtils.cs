5cb1856558af619760e2f83ce285c5404417f1c7
ritikdave <ritik.dave9@intimetec.com>
2025-05-26 16:27:08 +0530

    Unit Test Assignment

---
+++ UnitTestAssignment/UnitTestAssignment/DivisorUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public static class DivisorUtils
+{
+    public static int CountMatchingDivisors(int n)
+    {
+        if (n <= 0) return 0;
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int a = i;
+            int b = n - i + 1;
+
+            if (CountDivisors(a) == CountDivisors(b))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int CountDivisors(int number)
+    {
+        int count = 0;
+        int sqrt = (int)Math.Sqrt(number);
+
+        for (int i = 1; i <= sqrt; i++)
+        {
+            if (number % i == 0)
+            {
+                count += (i * i == number) ? 1 : 2;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ UnitTestAssignment/UnitTestAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = DivisorUtils.CountMatchingDivisors(n);
+            Console.WriteLine(result);
+        }
+    }
+}
