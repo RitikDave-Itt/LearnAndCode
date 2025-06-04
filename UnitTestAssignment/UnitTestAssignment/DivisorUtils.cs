7c0ed3a6bc66b51876cf9f67c9ed4b94f13d1c34
ritikdave <ritik.dave9@intimetec.com>
2025-06-04 17:02:36 +0530

    then inplemented the method now all unit tests are passing

--- UnitTestAssignment/UnitTestAssignment/DivisorUtils.cs
+++ UnitTestAssignment/UnitTestAssignment/DivisorUtils.cs
@@ -8,7 +8,16 @@ public static class DivisorUtils
 
         int count = 0;
 
-        
+        for (int i = 1; i <= n; i++)
+        {
+            int a = i;
+            int b = n - i + 1;
+
+            if (CountDivisors(a) == CountDivisors(b))
+            {
+                count++;
+            }
+        }
 
         return count;
     }
